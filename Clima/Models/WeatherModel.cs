335bb50d965980a97f68d8a6a5d64720f25c223a
Rolando José Garcia Escobar <rolaescobar@gmail.com>
2024-11-09 18:25:38 -0600

    Agregar archivos de proyecto.

---
+++ Clima/Models/WeatherModel.cs
@@ -0,0 +1,19 @@
+﻿namespace Clima.Models
+{
+    public class WeatherModel
+    {
+        public double Temperatura { get; set; }
+
+        public string? Descripcion { get; set; } 
+
+        public int Humedad {  get; set; }
+
+        public double Viento { get; set; }
+
+        public string? Ciudad {  get; set; }
+
+        public string? Pais { get; set; }
+
+
+    }
+}
